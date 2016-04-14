bf882bee6d09774482776529336ad55e019da0d5
student <student@debian>
2016-04-14 16:41:13 +0200

    v1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,59 @@
 #include <stdio.h>
+#include <string>
+
+
 
 int main(int argc, char *argv[])
 {
-	printf("Hello, World!\n");
-}
+	unsigned long long int number;
+	number=123456;
+
+	int digit[6];
+
+	for(int i=0, k=1; i<6; i++, k=k*10)
+	{
+		digit[i]=(number/k)%10;
+
+		printf("%u \n", digit[i]);
+
+	};
+
+	if(digit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+	std::string word[40];
+	word[0]="jeden";
+	word[1]="dwa";
+	word[2]="trzy";
+	word[3]="cztery";
+	word[4]="pięc";
+	word[5]="sześć";
+	word[6]="siedem";
+	word[7]="osiem";
+	word[8]="dziewięć";
+	word[9]="dziesięć";
+	word[10]="jedenaście";
+	word[11]="dwanaście";
+	word[12]="trzynaście";
+	word[13]="czternaście";
+	word[14]="piętnaście";
+	word[15]="szesnaście";
+	word[16]="siedemnaście";
+	word[17]="osiemnaście";
+	word[18]="dziewiętnaście";
+	word[19]="dwadzieścia";
+	word[20]
+
+
+};
